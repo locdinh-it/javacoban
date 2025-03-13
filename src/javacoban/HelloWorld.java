8537190c17a5dd5465d7383a0c16ea10d5ea93fe
loc <loc@dh.local>
2025-03-13 22:25:16 +0700

    day 1

--- src/javacoban/HelloWorld.java
+++ src/javacoban/HelloWorld.java
@@ -3,5 +3,7 @@ package javacoban;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello loc");
+		
+		System.out.printf("so le la : %.2f", 3000.222356);
 	}
 }
